7a4aa00341b8d727b77c3df8c1e590422031fa93
Lokesh Jain <56452497+jainlokesh318@users.noreply.github.com>
2021-01-07 17:01:04 +0530

    mvp done

---
+++ app.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const upBtn = document.querySelector('#up');
+    const leftBtn = document.querySelector('#left');
+    const downBtn = document.querySelector('#down');
+    const rightBtn = document.querySelector('#right');
+
+    const startBtn = document.querySelector('#start');
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+
+    let message = document.querySelector('#message');
+    const warning = "You are on the boundary";
+    const greeting = "Let's Play!"
+
+    const width = 10;
+    let currentPosition = 99;
+    let currentRow = 9;
+    let currentColumn = 9; 
+
+    function draw() {
+      squares[currentPosition].classList.add('currentBlock');  
+    }
+
+    function undraw() {
+        squares[currentPosition].classList.remove('currentBlock');
+    }
+
+    startBtn.addEventListener('click', () => {
+        draw();
+    });
+
+    upBtn.addEventListener('click', () => {
+        if(currentColumn >= 0 && currentRow > 0){
+            undraw();
+            currentPosition -= width;
+            draw();
+            currentRow--;
+            message.innerHTML = greeting;
+        } else {
+            message.innerHTML = warning;
+        }
+    });
+    
+    leftBtn.addEventListener('click', () => {
+        if(currentColumn > 0 && currentRow >= 0)
+        {
+            undraw();
+            currentPosition -= 1;
+            draw();
+            currentColumn--;
+            message.innerHTML = greeting;
+        } else {
+            message.innerHTML = warning;
+        }
+    });
+
+    
+    downBtn.addEventListener('click', () => {
+        if(currentRow < 9)
+        {
+            undraw();
+            currentPosition += width;
+            draw();
+            currentRow++;
+            message.innerHTML = greeting;
+        } else {
+            message.innerHTML = warning;
+        }
+    });
+
+    
+    rightBtn.addEventListener('click', () => {
+        if(currentColumn < 9) {
+            undraw();
+            currentPosition += 1;
+            draw();
+            currentColumn++;
+            message.innerHTML = greeting;
+        } else {
+            message.innerHTML = warning;
+        }
+    });
+});
